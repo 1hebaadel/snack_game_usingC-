f67c318c4764cc4428a40554a02e4898ef49ccd8
1hebaadel <hebaadelahmedmohammed@gmail.com>
2023-03-24 22:46:25 +0200

    first commit

---
+++ SnackGame/Program.cs
@@ -0,0 +1,16 @@
+namespace Snack_Game
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
